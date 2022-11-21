919433041897f20efc6f5577faabb019744a41de
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-11-21 20:39:07 -0300

    settings dependencies

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_netflix_responsive_ui/assets.dart';
-import 'package:flutter_netflix_responsive_ui/models/models.dart';
+import 'assets.dart';
+import '../models/models.dart';
 
-final Content sintelContent = Content(
+
+const Content sintelContent = Content(
   name: 'Sintel',
   imageUrl: Assets.sintel,
   titleImageUrl: Assets.sintelTitle,
   videoUrl: Assets.sintelVideoUrl,
   description:
-      'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+      'Uma jovem solitária, Sintel, ajuda e faz amizade com um dragão,\na quem ela chama de Scales. Mas quando ele é sequestrado por um dragão adulto, Sintel decide embarcar em uma perigosa busca para encontrar seu amigo perdido Scales.',
 );
 
-final List<Content> previews = const [
+const List<Content> previews = [
   Content(
     name: 'Avatar The Last Airbender',
     imageUrl: Assets.atla,
@@ -74,7 +75,7 @@ final List<Content> previews = const [
   ),
 ];
 
-final List<Content> myList = const [
+const List<Content> myList = [
   Content(name: 'Violet Evergarden', imageUrl: Assets.violetEvergarden),
   Content(name: 'How to Sell Drugs Online (Fast)', imageUrl: Assets.htsdof),
   Content(name: 'Kakegurui', imageUrl: Assets.kakegurui),
@@ -87,7 +88,7 @@ final List<Content> myList = const [
   Content(name: 'Black Mirror', imageUrl: Assets.blackMirror),
 ];
 
-final List<Content> originals = const [
+const List<Content> originals = [
   Content(name: 'Stranger Things', imageUrl: Assets.strangerThings),
   Content(name: 'The Witcher', imageUrl: Assets.witcher),
   Content(name: 'The Umbrella Academy', imageUrl: Assets.umbrellaAcademy),
@@ -100,7 +101,7 @@ final List<Content> originals = const [
   Content(name: 'The End of the F***ing World', imageUrl: Assets.teotfw),
 ];
 
-final List<Content> trending = const [
+const List<Content> trending = [
   Content(name: 'Explained', imageUrl: Assets.explained),
   Content(name: 'Avatar The Last Airbender', imageUrl: Assets.atla),
   Content(name: 'Tiger King', imageUrl: Assets.tigerKing),
